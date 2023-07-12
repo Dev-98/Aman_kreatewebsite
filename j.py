bc740856144ea8934c1ef43a458e9c982cd51a5d
Dev <90096363+Dev-98@users.noreply.github.com>
2023-07-12 15:23:05 +0530

    Add files via upload
    testing purpose

---
+++ j.py
@@ -0,0 +1,24 @@
+# import streamlit as st
+
+# # with st.sidebar.form("my_form"):
+# #     st.write("Inside the form")
+# #     slider_val = st.sidebar.slider("Form slider")
+# #     checkbox_val = st.sidebar.checkbox("Form checkbox")
+
+# #     # Every form must have a submit button.
+# #     sub = st.sidebar.form_submit_button()
+
+# # with st.sidebar:
+# form = st.sidebar.form("my_form")
+# a = float(form.slider("Inside the form"))
+# checkbox_val = form.checkbox("Form checkbox")
+# # form.write('sss',checkbox_val)
+# st.checkbox("Outside the form")
+
+# # Now add a submit button to the form:
+# form.form_submit_button("Submit")
+# # st.write("Outside the form")
+# st.write('check',a)
+# # st.slider("Form slider")
+import pip
+print(pip.__version__)
